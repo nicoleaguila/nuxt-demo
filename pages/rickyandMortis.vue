bcdff45df3051074369f1ba232b1ba0a89fbfe74
nicoleaguila <aguilanicole92@gmail.com>
2020-09-05 00:21:08 -0400

    usobasicodenuxt

---
+++ pages/rickyandMortis.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <v-flex d-flex>
+      <v-layout wrap class="mx-10">
+        <v-flex v-for="personaje in personajes" :key="personaje.id" md4>
+          <v-card class="card-container mt-8 mx-1 pb-2" max-width="500px">
+            <v-img :src="personaje.imagen" height="100px width:400px" class="ma-auto" />
+
+            <v-card-title class="pt-3">{{ personaje.nombre }} </v-card-title>
+            </v-card>
+          </v-flex>
+    </v-layout>
+    </v-flex>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rickyandMortis',
+  data () {
+    return {
+      personajes: [
+        {
+          id: 1,
+          nombre: 'Morty',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+        },
+        {
+          id: 2,
+          nombre: 'Rick',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+        },
+        {
+          id: 3,
+          nombre: 'Summer',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+        },
+        {
+          id: 4,
+          nombre: 'Beth',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+        },
+        {
+          id: 5,
+          nombre: 'Jerry',
+          imagen: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg'
+        }
+      ]
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
